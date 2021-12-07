e5a00fa6367f3eb432415b6a6ba23c5fba8e5ebe
MixBars <portalmixbars@gmail.com>
2021-12-07 05:44:47 -0800

    201

--- test.rb
+++ test.rb
@@ -5,7 +5,7 @@ require 'ohm'
 Ohm.redis = Redic.new('redis://127.0.0.1:6379')
 
 post '/set' do
-  Ohm.redis.call 'SET', params[:key], params[:value]
+  status 201 if Ohm.redis.call 'SET', params[:key], params[:value]
 end
 
 get '/get' do
