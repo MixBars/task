b96f6eedab61813101a40238b9220fc207637d96
MixBars <portalmixbars@gmail.com>
2021-12-07 04:55:22 -0800

    rubocoped -a

--- test.rb
+++ test.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'ohm'
-Ohm.redis = Redic.new("redis://127.0.0.1:6379")
+Ohm.redis = Redic.new('redis://127.0.0.1:6379')
 
 post '/set' do
- Ohm.redis.call "SET", params[:key], params[:value]
+  Ohm.redis.call 'SET', params[:key], params[:value]
 end
 
 get '/get' do
-  Ohm.redis.call "GET", params[:key]
+  Ohm.redis.call 'GET', params[:key]
 end
-
